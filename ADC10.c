9e8b7a051209a847b3fd669b075d41e6a759f5b8
RyanRedhead <C16Ryan.Redhead@usafa.edu>
2014-12-08 21:56:03 -0700

    ADC10 Library

---
+++ ADC10.c
@@ -0,0 +1,36 @@
+
+
+/*
+ * main.c
+ */
+
+#include "ADC10.h"
+#include <msp430.h>
+
+int readvalue(int pinnumber) {
+
+	BCSCTL1 = CALBC1_8MHZ;									// 8MHz clock
+	DCOCTL = CALDCO_8MHZ;
+
+	ADC10CTL0 = 0;									//P1.4
+	ADC10CTL1 = ADC10DIV_3 ;
+	ADC10CTL0 = SREF_0 | ADC10SHT_3 | ADC10ON | ENC;
+
+	if (pinnumber == 2){
+		ADC10CTL1 = INCH_2;									// Channel 0
+		ADC10AE0 = BIT2;
+	}
+	else if (pinnumber == 3){
+		ADC10CTL1 = INCH_3;									// Channel 0
+		ADC10AE0 = BIT3;
+	}
+	else if (pinnumber == 4){
+		ADC10CTL1 = INCH_4;									// Channel 0
+		ADC10AE0 = BIT4;
+	}
+
+	ADC10CTL0 |= ADC10SC;									// Start a conversion
+	while(ADC10CTL1 & ADC10BUSY);							// Wait for conversion to complete
+	return ADC10MEM;
+
+}
---
+++ ADC10.h
@@ -0,0 +1,13 @@
+/*
+ * ADC10.h
+ *
+ *  Created on: Dec 8, 2014
+ *      Author: C16Ryan.Redhead
+ */
+
+#ifndef ADC10_H_
+#define ADC10_H_
+
+int readvalue(int pinnumber);
+
+#endif /* ADC10_H_ */
