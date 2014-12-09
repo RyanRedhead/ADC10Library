e3f70fdf039f87c9fc2b583c42de08bd5a3dc2d2
RyanRedhead <C16Ryan.Redhead@usafa.edu>
2014-12-08 23:27:31 -0700

    Update ADC10.h

--- ADC10.h
+++ ADC10.h
@@ -2,12 +2,12 @@
  * ADC10.h
  *
  *  Created on: Dec 8, 2014
- *      Author: C16Ryan.Redhead
+ *      Author: Ryan Redhead
  */
 
 #ifndef ADC10_H_
 #define ADC10_H_
 
-unsigned short readvalue(int pinnumber);
+unsigned short readvalue(int pinnumber); //readvalue will depend on the pin being used
 
 #endif /* ADC10_H_ */
